993ce6bbd3f3a19d3c0210a138cd43f6c46ff659
blaiseludvig <blaiseludvig@gmail.com>
2022-10-06 12:24:08 +0200

    setup project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./style.css";
-// import { animateCSS } from "./animatecss";
+import { animateCSS } from "./animatecss";
 
 let element = document.createElement('h1');
 element.textContent = "Ha ezt látod, akkor működik!"
